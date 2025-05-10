3f8f348a72aac495b42b2fcbc58ac555ce19e161
coxde <63153334+coxde@users.noreply.github.com>
2025-05-10 17:33:08 +0100

    Revert "feat(pkg): add scopebuddy", won't work for flatpak for now
    This reverts commit 342263ff128e5b898c724c15f4c1d7b9ee0ab4e8.

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -11,15 +11,6 @@ dnf5 -y install \
 # Install COPR packages
 
 # Install 3rd party packages
-## Install ScopeBuddy
-curl -Lo /tmp/scopebuddy.tar.gz https://github.com/HikariKnight/ScopeBuddy/archive/refs/tags/$(curl https://api.github.com/repos/HikariKnight/scopebuddy/releases/latest | jq -r '.tag_name').tar.gz
-mkdir -p /tmp/scopebuddy
-tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/scopebuddy.tar.gz -C /tmp/scopebuddy
-rm -f /tmp/scopebuddy.tar.gz
-
-for file in /tmp/scopebuddy/ScopeBuddy-*/bin/*; do
-  install -Dm0755 "$file" /usr/bin/
-done
 
 # Remove packages
 dnf5 -y remove \
