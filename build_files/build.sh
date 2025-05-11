0219618f5cf14dc24b3372e5a412d22cf80f0aef
coxde <63153334+coxde@users.noreply.github.com>
2025-05-11 19:50:17 +0100

    chore: refactor build files

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,28 +2,25 @@
 
 set -ouex pipefail
 
+echo "::group:: ===== Populate Image Info ====="
+/ctx/build_files/00-image-info.sh
+echo "::endgroup::"
+
 echo "::group:: ===== Manage Packages ====="
-/ctx/build_files/packages.sh
+/ctx/build_files/01-packages.sh
+/ctx/build_files/02-icons.sh
 install -Dm0644 -t /etc/ublue-os/ /ctx/iso_files/*.list # ujust install-system-flatpaks
 echo "::endgroup::"
 
-echo "::group:: ===== Run Scripts ====="
-/ctx/build_files/flatpak-librewolf.sh
+echo "::group:: ===== Manage Configurations ====="
+/ctx/build_files/10-flatpak-librewolf.sh
 rsync -rvK /ctx/system_files/ /
 echo "::endgroup::"
 
-echo "::group:: ===== Install Themes ====="
-/ctx/build_files/icons.sh
-echo "::endgroup::"
-
-echo "::group:: ===== Include Just Recipes ====="
-/ctx/build_files/just.sh
-echo "::endgroup::"
-
-echo "::group:: ===== Replace Image Info ====="
-/ctx/build_files/image-info.sh
+echo "::group:: ===== Include Justfiles ====="
+/ctx/build_files/20-just.sh
 echo "::endgroup::"
 
 echo "::group:: ===== Finalize ====="
-/ctx/build_files/finalize.sh
+/ctx/build_files/99-finalize.sh
 echo "::endgroup::"
