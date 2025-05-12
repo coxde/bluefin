de4fca3e7e00f933c17bb9aa862ec1689b52d11b
coxde <63153334+coxde@users.noreply.github.com>
2025-05-12 10:53:20 +0100

    feat(pkg): add adwaita as papirus fallback

--- build_files/02-icons.sh
+++ build_files/02-icons.sh
@@ -4,3 +4,14 @@ set -ouex pipefail
 
 # Install Papirus Icon Theme
 wget -qO- https://git.io/papirus-icon-theme-install | sh
+
+# Add Adwaita as fallback
+files=(
+    "/usr/share/icons/Papirus/index.theme"
+    "/usr/share/icons/Papirus-Dark/index.theme"
+    "/usr/share/icons/Papirus-Light/index.theme"
+)
+
+for file in "${files[@]}"; do
+    sed -i "s/\(Inherits=\)/\1Adwaita,/g" "$file"
+done
